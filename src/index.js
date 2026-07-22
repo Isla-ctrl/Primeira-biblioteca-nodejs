33d599e38bb044e6733331f0a21a3c91e8c15c2d
Isla-ctrl <bzsouza1303@gmail.com>
2026-07-22 12:25:12 -0300

    Le arquivo de texto com fs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+//Require - importa módulos e bibliotecas do Node.js
+//fs - módulo do Node.js para manipulação de arquivos
+const fs = require('fs');
+
+//process.argv - array que contém os argumentos passados para o script Node.js
 const caminhoArquivo = process.argv;
+//caminhoArquivo[2] - terceiro argumento passado para o script, que é o caminho do arquivo a ser lido
 const link = caminhoArquivo[2];
 
-console.log(link);
+fs.readFile(link, 'utf8', (err, data) => {
+  if (err) {
+    console.log('Erro ao ler o arquivo:', err);
+    return;
+  }
+  console.log(data);
+});
+
