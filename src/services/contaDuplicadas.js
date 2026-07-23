587fd56a295e759eca802463a60ac4eb0264eebf
Isla-ctrl <bzsouza1303@gmail.com>
2026-07-23 19:48:17 -0300

    Adiciona filtragem de palavras com mais 2 letras

--- src/services/contaDuplicadas.js
+++ src/services/contaDuplicadas.js
@@ -8,7 +8,9 @@ export default function encontrarPalavrasDuplicadas(texto) {
         .replace(/[^a-zA-Zà-úÀ-Ú\s]/g, ''); // Remove caracteres que não sejam letras ou espaços (é uma expressão regular ou regex e existe formas bem variadas de escrever uma regex)
     //split - divide o texto em um array de palavras, usando espaço como separador e selecionador das palavras
     //filter(Boolean) - remove strings vazias do array e é nossa rede de segurança.
-    const palavras = textoLimpo.split(/\s+/).filter(Boolean);
+    const palavras = textoLimpo
+        .split(/\s+/)
+        .filter((palavra) => Boolean(palavra) && palavra.length > 2); // Filtra palavras com mais de 2 letras
     //reduce - reduz o array de palavras criando um objeto que conta a quantidade de vezes que cada palavra aparece. O acumulador é um objeto onde as chaves são as palavras e os valores são as contagens.
     //{} - diz para o reduce que o valor inicial do acumulador é um objeto vazio
     const contagem = palavras.reduce((acumulador, palavra) => {
