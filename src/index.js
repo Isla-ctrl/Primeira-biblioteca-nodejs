a42a407e2e286bf0090ee5551f9a7190106fda81
Isla-ctrl <bzsouza1303@gmail.com>
2026-07-23 20:24:09 -0300

    Adiciona funcao de salvar resultado de duplicadas em um novo arquivo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import encontrarPalavrasDuplicadas from './services/contaDuplicadas.js';
 import exibirDuplicadas from './views/cli.js';
 import exibirErro from './utils/funcaoErro.js';
-
 //Require - importa módulos e bibliotecas do Node.js
 //fs - módulo do Node.js para manipulação de arquivos
 import fs from 'fs';
@@ -11,6 +10,7 @@ import path from 'path';
 async function lerArquivo() {
 //process.argv - array que contém os vetores de argumento passados para o script nodejs. O índice 2 é usado para acessar o terceiro argumento, que é o caminho do arquivo fornecido pelo usuário.
 const caminhoArquivo = process.argv[2];
+const enderecoNovo = process.argv[3];
 
 if(!caminhoArquivo) {
     exibirErro('Por favor, informe o caminho do arquivo! EX: node src/index.js ./arquivo/texto.txt');
@@ -20,12 +20,25 @@ if(!caminhoArquivo) {
 try {
     //path.resolve - converte o caminho relativo do arquivo em um caminho absoluto, garantindo que o arquivo seja encontrado corretamente, independentemente de onde o script seja executado.
     const caminhoAbsoluto = path.resolve(caminhoArquivo);
+    const endereco = path.resolve(enderecoNovo);
     //fs.promises.readFile - lê o conteúdo do arquivo de forma assíncrona, retornando uma Promise que resolve com o conteúdo do arquivo. O segundo argumento 'utf8' garante que o conteúdo seja lido como uma string.
     const texto = await fs.promises.readFile(caminhoAbsoluto, 'utf8');
     const duplicadas = encontrarPalavrasDuplicadas(texto);
     exibirDuplicadas(duplicadas);
+    criaESalvaArquivo(duplicadas, endereco);
 } catch (erro) {
     exibirErro(`O arquivo no caminho "${caminhoArquivo}" não existe.`, erro.message);
+    }
+}
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textoPalavras = JSON.stringify(listaPalavras);
+    try {
+        await fs.promises.writeFile(arquivoNovo, textoPalavras);
+        console.log('Arquivo criado') 
+    } catch (erro) {
+        throw erro;
     }
 }
 
