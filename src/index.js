674598e1e898aa22d1a4e220fce2f60789fd34ce
Isla-ctrl <bzsouza1303@gmail.com>
2026-07-27 16:40:34 -0300

    Adiciona e implementa lib externa do commander

--- src/index.js
+++ src/index.js
@@ -2,39 +2,57 @@ import encontrarPalavrasDuplicadas from './services/contaDuplicadas.js';
 import exibirDuplicadas from './views/cli.js';
 import exibirErro from './utils/funcaoErro.js';
 import resultadoArquivoEstilizado from './utils/helper.js';
+import { Command } from 'commander';
 //Require - importa módulos e bibliotecas do Node.js
 //fs - módulo do Node.js para manipulação de arquivos
 import fs from 'fs';
 //path - módulo para manipulação de caminhos de arquivos e diretórios, roda em qualquer sistema sem dar erro de caminho
 import path from 'path';
 
-async function lerArquivo() {
-//process.argv - array que contém os vetores de argumento passados para o script nodejs. O índice 2 é usado para acessar o terceiro argumento, que é o caminho do arquivo fornecido pelo usuário.
-const caminhoArquivo = process.argv[2];
-const enderecoNovo = process.argv[3];
+const program = new Command();
 
-if(!caminhoArquivo) {
-    exibirErro('Por favor, informe o caminho do arquivo! EX: node src/index.js ./arquivo/texto.txt');
-    return;
-}
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'Caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'Caminho do diretório de destino para salvar o arquivo de resultado')
+    .action(async (options) => {
+        const { texto, destino } = options;
 
-try {
-    //path.resolve - converte o caminho relativo do arquivo em um caminho absoluto, garantindo que o arquivo seja encontrado corretamente, independentemente de onde o script seja executado.
-    const caminhoAbsoluto = path.resolve(caminhoArquivo);
-    const endereco = path.resolve(enderecoNovo);
-    //fs.promises.readFile - lê o conteúdo do arquivo de forma assíncrona, retornando uma Promise que resolve com o conteúdo do arquivo. O segundo argumento 'utf8' garante que o conteúdo seja lido como uma string.
-    const texto = await fs.promises.readFile(caminhoAbsoluto, 'utf8');
-    const duplicadas = encontrarPalavrasDuplicadas(texto);
-    exibirDuplicadas(duplicadas);
-    criaESalvaArquivo(duplicadas, endereco);
-} catch (erro) {
-    exibirErro(`O arquivo no caminho "${caminhoArquivo}" não existe.`, erro.message);
+        if (!texto || !destino) {
+            exibirErro('Por favor informe o caminho do arquivo de texto e o diretório de destino. EX: node src/index.js -t arquivo/texto-web.txt -d ./resultados');
+            program.help();
+            return;
+        }
+        //path.resolve - converte o caminho relativo do arquivo em um caminho absoluto, garantindo que o arquivo seja encontrado corretamente, independentemente de onde o script seja executado.
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+
+        try {
+            lerArquivo(caminhoTexto, caminhoDestino);
+            console.log('Processamento concluído com sucesso.');
+        } catch (erro) {
+            exibirErro('Falha ao processar o arquivo.', erro.message);
+        }
+    });
+
+program.parse(process.argv);
+
+async function lerArquivo(caminhoTexto, caminhoDestino) {
+//fs.promises.readFile - lê o conteúdo do arquivo de forma assíncrona, retornando uma Promise que resolve com o conteúdo do arquivo. O segundo argumento 'utf8' garante que o conteúdo seja lido como uma string.
+    try {
+        const conteudoTexto = await fs.promises.readFile(caminhoTexto, 'utf8');
+        const duplicadas = encontrarPalavrasDuplicadas(conteudoTexto);
+        exibirDuplicadas(duplicadas);
+        await criaESalvaArquivo(duplicadas, caminhoDestino);
+    } catch (erro) {
+        throw erro;
     }
 }
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
-    const arquivoNovo = `${endereco}/resultado.txt`;
+    const arquivoNovo = path.join(endereco, 'resultado.txt');
     const textoPalavras = resultadoArquivoEstilizado(listaPalavras);    
+
     try {
         await fs.promises.writeFile(arquivoNovo, textoPalavras);
         console.log('Arquivo criado') 
@@ -42,5 +60,3 @@ async function criaESalvaArquivo(listaPalavras, endereco) {
         throw erro;
     }
 }
-
-lerArquivo();
